789165881a3ae699dd233b9985d1ee2bb6123711
Joanna May <joannamadisonmay@gmail.com>
2023-05-03 22:14:21 -0500

    test: add missing bloc glue test

--- Bloc.NET.Tests/test/src/utilities/BlocGlueFakeBloc.cs
+++ Bloc.NET.Tests/test/src/utilities/BlocGlueFakeBloc.cs
@@ -14,7 +14,13 @@ public partial class BlocGlueTests {
     public record StateB(string Value1, string Value2) : FakeBlocState;
   }
 
-  public class FakeBloc : SyncBlocClassic<IFakeBlocEvent, FakeBlocState> {
+  public interface IFakeBlocEffect {
+    public record struct EffectOne(int Value) : IFakeBlocEffect;
+    public record struct EffectTwo(string Value) : IFakeBlocEffect;
+  }
+
+  public class FakeBloc
+    : SyncBloc<IFakeBlocEvent, FakeBlocState, IFakeBlocEffect> {
     public FakeBloc() : base(new FakeBlocState.StateA(1, 2)) {
       On<IFakeBlocEvent.EventOne>(One);
       On<IFakeBlocEvent.EventTwo>(Two);
@@ -23,12 +29,14 @@ public partial class BlocGlueTests {
     private IEnumerable<FakeBlocState> One(
       IFakeBlocEvent.EventOne @event
     ) {
+      Trigger(new IFakeBlocEffect.EffectOne(1));
       yield return new FakeBlocState.StateA(@event.Value1, @event.Value2);
     }
 
     private IEnumerable<FakeBlocState> Two(
       IFakeBlocEvent.EventTwo @event
     ) {
+      Trigger(new IFakeBlocEffect.EffectTwo("2"));
       yield return new FakeBlocState.StateB(@event.Value1, @event.Value2);
     }
   }
